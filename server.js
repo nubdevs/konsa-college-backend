528fae0ebb6d7835114a6eb39dbdadeb9ccc2d3d
nubdevs <idkbharti@gmail.com>
2023-01-11 23:36:35 -0800

    commit

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get("/add",(req,res)=>{
   // });
 })
 
-const PORT=process.env.PORT
+const PORT=process.env.PORT || 5000
 app.listen(PORT, function() {
   console.log(`Server started on port ${PORT}`);
 });
